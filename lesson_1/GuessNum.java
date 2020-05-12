40cecbd1cdf33a2b760b4673e1941e7e0605e8d6
TkachevArtem <art599@yandex.ru>
2020-05-12 15:05:04 +0300

    HW GuessNum

---
+++ lesson_1/GuessNum.java
@@ -0,0 +1,16 @@
+public class GuessNum {
+	public static void main(String[] args) {
+		int guessNum = 47;
+
+		for(int i = 0;  i <= 100; i++) {
+			if(i < guessNum) {
+				System.out.println("Введенное вами число: " + i + " меньше того, что загадал компьютер");
+			} else if(i > guessNum) {
+				System.out.println("Введенное вами число " + i + " больше того, что загадал компьютер");
+			} else if(i == guessNum) {
+				System.out.println("Вы угадали. Это число " + i);
+				break;
+			}
+		}
+	}
+}
