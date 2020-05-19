2c7627a2017cf0d6491f9fd14bae95bf1808b199
TkachevArtem <art599@yandex.ru>
2020-05-19 15:00:34 +0300

    HW_Peron

---
+++ lesson_1/Person.java
@@ -0,0 +1,28 @@
+public class Person {
+	
+	char sex = 'm';
+	String name = "Artem";
+	int height = 185;
+	int weight = 90;
+	int age = 32;
+
+	void go() {
+
+	};
+
+	void sit() {
+
+	}
+
+	void run() {
+
+	}
+
+	String talk() {
+		return "What?";
+	}
+
+	void learnJava() {
+		
+	}
+}
