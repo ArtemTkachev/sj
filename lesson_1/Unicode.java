db498db90d24525180d8cd3d10588fe3bf0f03ab
TkachevArtem <art599@yandex.ru>
2020-05-19 15:28:59 +0300

    HW Unicode

--- lesson_1/Unicode.java
+++ lesson_1/Unicode.java
@@ -1,7 +1,7 @@
 public class Unicode {
 	public static void main(String[] args) {
 		char symbol = ' ';
-		
+
 		for(int i = 33; i <= 126; i++) {
 			symbol = (char)(i);
 			System.out.println(symbol);
