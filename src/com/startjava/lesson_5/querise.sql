5e5caa9842ab4af19a37108952e99109e61519be
TkachevArtem <art599@yandex.ru>
2020-06-12 15:45:22 +0300

    HW "DB 2.0"

--- src/com/startjava/lesson_5/querise.sql
+++ src/com/startjava/lesson_5/querise.sql
@@ -1,10 +1,10 @@
 \c jaegers;
 SELECT * FROM jaegers;
 SElECT * FROM jaegers WHERE status != 'destroyed';
-SELECT * FROM jaegers WHERE mark in ('Mark-1', 'Mark-6');
+SELECT * FROM jaegers WHERE mark IN ('Mark-1', 'Mark-6');
 SELECT * FROM jaegers ORDER BY mark DESC;
 SELECT * FROM jaegers ORDER BY launched LIMIT 1;
-SELECT * FROM jaegers WHERE kaijuKilled in ((SELECT MAX(kaijuKilled) FROM jaegers), (SELECT MIN(kaijuKilled) FROM jaegers));
+SELECT * FROM jaegers WHERE kaijuKilled IN ((SELECT MAX(kaijuKilled) FROM jaegers), (SELECT MIN(kaijuKilled) FROM jaegers));
 SELECT AVG(weight) AS averageWeight FROM jaegers;
 UPDATE jaegers SET kaijuKilled = kaijuKilled + 1 WHERE status != 'destroyed';
 DELETE FROM jaegers WHERE status = 'destroyed';
